a0b8e06001174a5faed56c4942f5a78f3fea2489
Thaw ye Zaw <thawyezaw@gmail.com>
2022-07-06 14:48:14 +0630

    commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App text-red-700">
+      hi
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,168 @@
+const iphone13 = [
+		{
+			name: 'blue',
+			color: '#3bbcff',
+			image: 'images/Iphone13/lblue.png',
+			onSale: true,
+		},
+		{
+			name: 'midnight',
+			color: '#192028',
+			image: 'images/Iphone13/midnight.png',
+			onSale: false,
+		},
+		{
+			name: 'starlight',
+			color: 'lightgrey',
+			image: 'images/Iphone13/starlight.png',
+			onSale: true,
+		},
+		{
+			name: 'pink',
+			color: '#FFC0CB',
+			image: 'images/Iphone13/pink.png',
+			onSale: false,
+		},
+		{
+			name: 'red',
+			color: '#AC0415',
+			image: 'images/Iphone13/red.png',
+			onSale: true,
+		},
+	],
+	iphone13pro = [
+		{
+			name: 'gold',
+			color: '#F2E92C',
+			image: 'images/Iphone13pro/gold.png',
+			onSale: true,
+		},
+		{
+			name: 'silver',
+			color: '#CFD0CA',
+			image: 'images/Iphone13pro/silver.png',
+			onSale: false,
+		},
+		{
+			name: 'sierra blue',
+			color: '#406ED0',
+			image: 'images/Iphone13pro/sierra_blue.png',
+			onSale: true,
+		},
+		{
+			name: 'graphite',
+			color: '#4A4945',
+			image: 'images/Iphone13pro/graphite.png',
+			onSale: false,
+		},
+	],
+	ipad = [
+		{
+			name: 'green',
+			color: '#12CB7F',
+			image: 'images/ipad/green.png',
+			onSale: true,
+		},
+		{
+			name: 'rose gold',
+			color: '#FC6B96',
+			image: 'images/ipad/rosegold.png',
+			onSale: false,
+		},
+		{
+			name: 'starlight',
+			color: '#C9CACE',
+			image: 'images/ipad/silver.png',
+			onSale: true,
+		},
+		{
+			name: 'sky blue',
+			color: '#6DE8FA',
+			image: 'images/ipad/skyblue.png',
+			onSale: false,
+		},
+		{
+			name: 'space grey',
+			color: '#4E525B',
+			image: 'images/ipad/spacegrey.png',
+			onSale: true,
+		},
+	],
+	applewatch = [
+		{
+			name: 'abyss blue',
+			color: '#3D4D64',
+			image: 'images/applewatch/abyssblue.jpg',
+			onSale: true,
+		},
+		{
+			name: 'green',
+			color: '#516951',
+			image: 'images/applewatch/green.jpg',
+			onSale: false,
+		},
+		{
+			name: 'midnight',
+			color: '#717882',
+			image: 'images/applewatch/midnight.jpg',
+			onSale: true,
+		},
+		{
+			name: 'starlight',
+			color: '#E0D6CD',
+			image: 'images/applewatch/starlight.jpg',
+			onSale: true,
+		},
+		{
+			name: 'red',
+			color: '#DE4C61',
+			image: 'images/applewatch/red.jpg',
+			onSale: false,
+		},
+	],
+	macbookpro = [
+		{
+			name: 'silver',
+			color: '#C9CACE',
+			image: 'images/macbookpro/silver.jpg',
+			onSale: false,
+		},
+		{
+			name: 'space grey',
+			color: '#4E525B',
+			image: 'images/macbookpro/spacegrey.jpg',
+			onSale: true,
+		},
+	],
+	airpodsmax = [
+		{
+			name: 'blue',
+			color: '#3D4D64',
+			image: 'images/airpodsmax/blue.jpg',
+			onSale: true,
+		},
+		{
+			name: 'green',
+			color: '#516951',
+			image: 'images/airpodsmax/green.jpg',
+			onSale: false,
+		},
+		{
+			name: 'pink',
+			color: '#FFC0CB',
+			image: 'images/airpodsmax/pink.jpg',
+			onSale: true,
+		},
+		{
+			name: 'silver',
+			color: '#E0D6CD',
+			image: 'images/airpodsmax/silver.jpg',
+			onSale: true,
+		},
+		{
+			name: 'spacegrey',
+			color: '#4E525B',
+			image: 'images/airpodsmax/spacegrey.jpg',
+			onSale: false,
+		},
+	]
