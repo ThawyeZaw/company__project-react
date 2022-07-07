df181fc26c8bf248c052cf37aee3af7401a1452e
Thaw ye Zaw <thawyezaw@gmail.com>
2022-07-07 12:37:26 +0630

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import './App.css'
+import images from './images2/images'
 
 function App() {
-  return (
-    <div className="App text-red-700">
-      hi
-    </div>
-  );
+	return (
+		<>
+			<div className='App h-screen bg-slate-100'>
+				<img src={images.photo1} className='sticky top-0 z-0 h-full w-full object-cover object-left lg:object-fill' alt='logo' />
+			</div>
+			<div className='z-10 h-screen bg-slate-600'></div>
+		</>
+	)
 }
 
-export default App;
+export default App
---
+++ src/images2/images.js
@@ -0,0 +1,6 @@
+const images = {
+  logo: require('./logo.png'),
+  photo1: require('./image1.png')
+}
+
+export default images
