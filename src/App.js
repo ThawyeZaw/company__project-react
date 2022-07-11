ef48774f583e8af00de45b43c380667e05ffa262
Thaw ye Zaw <thawyezaw@gmail.com>
2022-07-11 14:41:41 +0630

    commit

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
 			const nav = document.querySelector('nav')
 
 			if (window.scrollY > 10) {
-				nav.classList.add('bg-slate-200', 'bg-opacity-80', 'text-gray-800')
+				nav.classList.add('bg-slate-200', 'bg-opacity-80', 'text-gray-800', 'text-gray-700')
 			} else {
-				nav.classList.remove('bg-slate-200', 'bg-opacity-80', 'text-gray-800')
+				nav.classList.remove('bg-slate-200', 'bg-opacity-80', 'text-gray-800', 'text-gray-700')
 			}
 
 		}
