6f74881b6bf3bedfdc78c36130bb3dd5a95a8050
Thaw ye Zaw <thawyezaw@gmail.com>
2022-07-13 12:49:17 +0630

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import images from '../images/images'
+import { BiSearchAlt2 } from 'react-icons/bi'
 
 const Home = () => {
 	return (
 		<div className='home relative flex h-screen w-full items-center justify-center'>
 			<img className='absolute z-0 h-screen w-full object-cover object-top mix-blend-color' src={images.photo1} alt='loading' />
 			<h1 className='absolute flex flex-col items-center font-sans text-7xl font-extrabold text-gray-50'>
-				<p className='text-stroke text-center lg:text-8xl'>
+				<p className='text-stroke text-center xl:text-8xl'>
 					Let's build
 					<br className='md:hidden' />
 					&nbsp;the future
 					<br /> together!
 					<br />
 				</p>
-				<div className='relative mt-20 h-16 w-80 text-lg text-slate-700 drop-shadow-[0_15px_15px_rgba(0,0,0,0.5)] sm:w-96 sm:text-xl lg:h-20 lg:w-[30rem] lg:rounded-full lg:text-2xl'>
-					<input type={'text'} className='h-full w-full rounded-2xl py-2 px-2 sm:px-8 lg:rounded-full ' placeholder='Find prices and designs' />
-					<button className='absolute top-0 right-0 h-full rounded-r-2xl bg-yellow-700 px-4 text-2xl font-bold tracking-widest text-gray-100 sm:px-6 lg:rounded-r-full lg:px-8 lg:text-3xl'>Find</button>
+				<div className='relative mt-20 h-16 w-80 text-lg text-slate-700 drop-shadow-[0_15px_15px_rgba(0,0,0,0.5)] sm:w-96 sm:text-xl lg:h-20 lg:w-[30rem] lg:rounded-full lg:text-2xl xl:h-24 xl:w-[35rem] xl:text-3xl'>
+					<input type={'text'} className='h-full w-full rounded-2xl py-2 px-4 sm:px-8 ' placeholder='Find prices and designs' autoFocus />
+					<button className='absolute top-0 right-0 h-full rounded-r-2xl bg-yellow-700 px-4 text-4xl font-bold tracking-widest text-gray-100 sm:px-6 lg:px-8 lg:text-5xl'>
+						<BiSearchAlt2 />
+					</button>
 				</div>
 			</h1>
 		</div>
--- src/components/Work.js
+++ src/components/Work.js
@@ -4,18 +4,18 @@ import images from '../images/images'
 const Work = () => {
 	return (
 		<div className='work-bg-gradient relative w-full'>
-			<div className='flex-column relative md:flex'>
-				<div className='h-[40vh] w-[100vw] overflow-hidden shadow-2xl md:h-[60vh] md:w-[50vw]'>
-					<img src={images.photo2} alt='loading' className='h-full w-full object-cover md:rounded-br-3xl' />
+			<div className='flex-column relative lg:flex'>
+				<div className='h-[40vh] w-[100vw] overflow-hidden shadow-2xl lg:h-[60vh] lg:w-[50vw]'>
+					<img src={images.photo2} alt='loading' className='h-full w-full object-cover lg:rounded-br-3xl' />
 				</div>
-				<div className='relative flex h-[40vh] w-[100vw] items-center justify-center md:h-full md:w-[50vw]'>
+				<div className='relative flex h-[40vh] w-[100vw] items-center justify-center lg:h-full lg:w-[50vw]'>
 					<div className='absolute top-0 flex h-full w-full justify-center sm:items-center'>
 						<img src={images.bg1} alt=' ' className='h-3/4' />
 					</div>
-					<h1 className='text-stroke w-3/4 items-center text-[6.5vw] font-extrabold text-gray-50 md:text-[3.3vw]'>Experienced Engineers</h1>
+					<h1 className='text-stroke w-3/4 items-center text-[6.5vw] font-extrabold text-gray-50 lg:text-[3.3vw]'>Experienced Engineers</h1>
 				</div>
 			</div>
-			<img src={images.photo2} alt='loading' className='mt-56 object-cover bg-blend-normal' />
+			<img src={images.photo2} alt='loading' className='mt-56 opacity-70 object-cover mix-blend-screen' />
 		</div>
 	)
 }
