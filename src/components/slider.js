84512ea577789295547358d6d27eda934f8c9563
Thaw ye Zaw <thawyezaw@gmail.com>
2022-07-19 16:24:40 +0630

    commit

--- src/components/slider.js
+++ src/components/slider.js
@@ -1,19 +1,19 @@
-import { images, slide_image } from '../images/images'
+import { slide_image } from '../images/images'
 
 const Slide = props => {
 	return (
-		<div className='w-[60vw] overflow-hidden rounded-lg shadow-lg shadow-slate-100'>
-			<img src={props.image} alt='Loading' />
+		<div className={'flex-shrink-0 overflow-hidden rounded-lg shadow-lg shadow-slate-100 inline-block ' + props.width}>
+			<img src={props.image} alt='Loading'  />
 		</div>
 	)
 }
 
 const Slider = () => {
 	return (
-		<div className='flex h-[75%] min-w-full items-center justify-center overflow-x-scroll'>
-			<Slide image={slide_image.slideImage1} />
-			<Slide image={slide_image.slideImage1} />
-			<Slide image={slide_image.slideImage1} />
+		<div className='flex h-[75%] gap-8 justify-center items-center scrolling-wrapper'>
+			<Slide image={slide_image.slideImage1} width={'w-[40vw]'} />
+			<Slide image={slide_image.slideImage1} width={'w-[60vw]'} />
+			<Slide image={slide_image.slideImage1} width={'w-[40vw]'} />
 		</div>
 	)
 }
